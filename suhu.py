62c5f098851a94a4e3422103b15ce66576c3f14c
Putradees <putradwisusanto01@gmail.com>
2022-09-18 14:36:37 +0700

    tambah farenheit

--- suhu.py
+++ suhu.py
@@ -12,3 +12,12 @@ if inputan.upper() == "C":
   jenis1 = "Fahrenheit"
   jenis2 = "Kelvin"
   jenis3 = "Reamur"
+
+elif inputan.upper() == "F":
+  hasil1 = float((drjt - 32) * 5 / 9)
+  hasil2 = float(((drjt - 32) * 5 / 9) + 273.15)
+  hasil3 = float(4/9 * (drjt-32))
+  jenisX = "Fahrenheit"
+  jenis1 = "Celsius"
+  jenis2 = "Kelvin"
+  jenis3 = "Reamur"
